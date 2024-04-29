f0f02d0148e11c6e1c775473e71802cb5f303165
CarolRamalh0 <146960854+CarolRamalh0@users.noreply.github.com>
2024-04-29 19:02:39 -0300

    Update port.js

--- port.js
+++ port.js
@@ -3,7 +3,15 @@ const slider = document.querySelector('.slider');
 function activate(e){
     const items = document.querySelectorAll('.item');
     e.target.matches('.next') && slider.append(items[0]);
-    e.target.matches('.prev') && slider.prepend(items[items.length-2]);
-}
+    e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+};
 
 document.addEventListener('click',activate,false);
+
+
+const toggleBtn = document.getElementById('toggle');
+const body = document.body;
+
+toggleBtn.addEventListener('click', function(){
+    body.classList.toggle('dark-mode')
+});
